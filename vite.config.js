8be1dc8f0b523ab4f33e1b07a67aa8b21b05cf91
Mario Guillen <marioagr95@gmail.com>
2023-07-28 20:17:04 -0600

    Added config to see live changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-    root: 'src',
+  plugins: [react()],
+  root: 'src',
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
 });
