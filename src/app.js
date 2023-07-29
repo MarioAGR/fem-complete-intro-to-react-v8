bdae40f2fbe3e61490f7d103f175edd09d1e16cf
Mario Guillen <marioagr95@gmail.com>
2023-07-28 18:52:41 -0600

    npm & Prettier setup

--- src/app.js
+++ src/app.js
@@ -7,28 +7,24 @@ const Pet = (props) => {
 };
 
 const App = () => {
-    return React.createElement(
-        'div',
-        {},
-        [
-            React.createElement('h1', {}, "Adopt Me!"),
-            React.createElement(Pet, {
-                name: 'Luna',
-                animal: 'Dog',
-                breed: 'Corgi'
-            }),
-            React.createElement(Pet, {
-                name: 'Pepper',
-                animal: 'Bird',
-                breed: 'Cockatiel'
-            }),
-            React.createElement(Pet, {
-                name: 'Doink',
-                animal: 'Cat',
-                breed: 'Mixed'
-            }),
-        ]
-    )
+    return React.createElement('div', {}, [
+        React.createElement('h1', {}, 'Adopt Me!'),
+        React.createElement(Pet, {
+            name: 'Luna',
+            animal: 'Dog',
+            breed: 'Corgi',
+        }),
+        React.createElement(Pet, {
+            name: 'Pepper',
+            animal: 'Bird',
+            breed: 'Cockatiel',
+        }),
+        React.createElement(Pet, {
+            name: 'Doink',
+            animal: 'Cat',
+            breed: 'Mixed',
+        }),
+    ]);
 };
 
 const container = document.getElementById('root');
